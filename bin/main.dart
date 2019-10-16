a839f42dbeb127198cadc2fe3a18a0883585cad7
xuzhongpeng <1452754335@qq.com>
2019-10-16 11:00:27 +0800

    这是信息

--- bin/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:git_hooks/git_hooks.dart' as git_hooks;
-
-main(List<String> arguments) {
-  print('Hello world: ${git_hooks.calculate()}!');
-  print('Hello world: ${arguments}!');
-}
